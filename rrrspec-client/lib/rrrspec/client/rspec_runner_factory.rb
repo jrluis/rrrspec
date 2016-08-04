f2d811603387801d647d29055709ef69fc8a8437
João Luís <jrluis@gmail.com>
2016-08-04 14:40:34 +0100

    save runner in thread local storage

--- rrrspec-client/lib/rrrspec/client/rspec_runner_factory.rb
+++ rrrspec-client/lib/rrrspec/client/rspec_runner_factory.rb
@@ -7,12 +7,15 @@ module RRRSpec
 
       def initialize(successor)
         @next_factory = successor
-        @runner = RSpecRunner.new
       end
 
       def create(file_extension)
-        return @runner if file_extension == '.rb'
-        return @runner if @next_factory == nil
+        unless Thread.current[:rspec3_runner]
+          Thread.current[:rspec3_runner] = RSpecRunner.new
+        end
+
+        return Thread.current[:rspec3_runner] if file_extension == '.rb'
+        return Thread.current[:rspec3_runner] if @next_factory == nil
 
         @next_factory.create file_extension
       end
