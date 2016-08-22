fcaa4e10bcb6036fca9d9631ddf5922cbf63982e
João Luís <jrluis@gmail.com>
2016-08-22 10:44:35 +0100

    moving rspec runner registration out of rrrspec.rb

--- rrrspec-client/lib/rrrspec/client/rspec_runner_factory.rb
+++ rrrspec-client/lib/rrrspec/client/rspec_runner_factory.rb
@@ -1,3 +1,4 @@
+require 'rrrspec/registry'
 require 'rrrspec/client/rspec_runner'
 
 module RRRSpec
@@ -12,5 +13,7 @@ module RRRSpec
         return @runner if file_extension == '.rb'
       end
     end
+
+    Registry.register_runner_factory RRRSpec::Client::RSpecRunnerFactory.new
   end
 end
